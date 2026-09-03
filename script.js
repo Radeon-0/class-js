144c259ad43e8ab1bccdf77231ff63841b2d66b4
Radion0 <radion.glushkov@mail.ru>
2026-09-03 17:30:16 +0700

    ДЗ УСЛОЖНЕННОЕ ЗАДАНИЕ

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-// dev
+let num = 266219;
+
+
+let digits = String(num).split('');
+let product = 1;
+
+for (let digit of digits) {
+  product *= Number(digit); 
+}
+
+console.log(product); 
+
+
+let pow= product ** 3;
+
+console.log(pow);
+
+
+let firstTwoDigits = String(pow).slice(0, 2);
+
+console.log(firstTwoDigits); 
