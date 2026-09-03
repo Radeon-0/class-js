540765f627dfcb43ae58db3b13625bf20bae10f3
Radion0 <radion.glushkov@mail.ru>
2026-09-03 15:25:07 +0700

    ДЗ

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-const title = "";
-const screens = "";
-const screenPrice = "";
-const rollback = "";
-const fullPrice = "";
-const adaptive = "";
+const title = "Алтай";
+const screens = "Простые, Сложные, Интерактивные";
+const screenPrice = "3500";
+const rollback = "55";
+const fullPrice = "100000";
+const adaptive = true;
 
-alert("lesson01");
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
 
-console.log("lesson01");
+
+console.log("Стоимость верстки экранов " + screenPrice + " рублей");
+console.log("Стоимость разработки сайта " + fullPrice + " рублей");
+
+console.log(screens.toLowerCase().split(", "));
+
+console.log(fullPrice * (rollback / 100));
